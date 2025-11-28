6c3bf691ec015a1f3b8dac6919970daa7a185345
Frank Sørensen <frank@clondyke.dk>
2025-11-28 16:15:25 +0100

    Added Ivy support and simpler buildscript and module-info.

---
+++ src/module-info.java
@@ -0,0 +1,37 @@
+module xercesImpl {
+
+    requires java.xml;
+    requires static xml.resolver;
+    requires jdk.xml.dom;
+    
+    
+    exports org.apache.xerces.dom;
+
+    uses javax.xml.parsers.DocumentBuilderFactory;
+    
+    uses javax.xml.parsers.SAXParserFactory;
+    
+    uses javax.xml.datatype.DatatypeFactory;
+    
+    uses javax.xml.validation.SchemaFactory;
+    
+    uses javax.xml.stream.XMLEventFactory;
+    
+    uses org.xml.sax.XMLReader;
+
+
+    provides javax.xml.parsers.DocumentBuilderFactory with org.apache.xerces.jaxp.DocumentBuilderFactoryImpl;
+
+    provides javax.xml.parsers.SAXParserFactory with org.apache.xerces.jaxp.SAXParserFactoryImpl;
+
+    provides javax.xml.datatype.DatatypeFactory with org.apache.xerces.jaxp.datatype.DatatypeFactoryImpl;
+    
+    provides javax.xml.validation.SchemaFactory with org.apache.xerces.jaxp.validation.XMLSchemaFactory;
+
+    provides javax.xml.stream.XMLEventFactory with org.apache.xerces.stax.XMLEventFactoryImpl;
+    
+    provides org.xml.sax.XMLReader with org.apache.xerces.parsers.SAXParser;
+    
+
+
+}
